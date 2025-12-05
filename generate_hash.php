b862147d12b491029568cb560c3672dec55cb28b
Carl Sharpe <carlsharpe87@gmail.com>
2025-12-05 01:24:37 -0500

    Created dolphin_crm folder to store final project files

--- generate_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Generate password hash for schema.sql
-$password = 'password123';
-$hash = password_hash($password, PASSWORD_DEFAULT);
-echo "Password hash for 'password123':\n";
-echo $hash;
-?>
