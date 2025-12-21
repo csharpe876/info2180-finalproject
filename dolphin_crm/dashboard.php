44239a1a34b6be899ba190e314f549ed2ce8ca68
Jabari McPherson <jabarithemosttalented@gmail.com>
2025-12-20 19:43:40 -0500

    Updated dashboard UI

--- dolphin_crm/dashboard.php
+++ dolphin_crm/dashboard.php
@@ -50,7 +50,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'get_contacts') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New User</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="includes/stylesheets/styling.css">
     <script src="script.js"></script>
 </head>
 <body>
